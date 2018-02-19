af5cce21836d8c0e57f69e4b80a0bd60c0eb64ee
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-02-20 03:34:38 +0400

    Update tel_tag helper

--- lib/more_html_tags/html_tags_helpers.rb
+++ lib/more_html_tags/html_tags_helpers.rb
@@ -1,5 +1,9 @@
 module HTMLTagsHelpers
   def tel_tag(telephone, html_options = nil, &block)
-    link_to telephone, "tel:#{telephone}", html_options, &block
+    if block_given?
+      link_to "tel:#{telephone}", html_options, &block
+    else
+      link_to telephone, "tel:#{telephone}", html_options
+    end
   end
 end
